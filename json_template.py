475400361c8f8a9aacbbcbd83393f49cf2a3c32d
Laasya Aki <101072841+laasyaaki@users.noreply.github.com>
2025-09-14 18:28:22 -0400

    Create json_template.py

---
+++ json_template.py
@@ -0,0 +1,52 @@
+# Template JSON
+{
+  "technique_name": "",
+  "technique_id": "D3-XXX",
+  "technique_definition": "",
+  "synonyms": [],
+  "preconditions": [
+    {
+      "input":
+      [
+        {
+          "precondition_id": "ID-PRE-001",
+          "prolog": "",
+          "description": ""
+        },
+        {
+          "precondition_id": "ID-PRE-002",
+          "prolog": "",
+          "description": ""
+        }
+      ],
+      "config":
+      [
+        {
+          "precondition_id": "ID-PRE-003",
+          "prolog": "",
+          "description": ""
+        }
+      ]
+    }
+  ],
+  "postconditions": [
+    {
+      "input":
+      [
+        {
+          "postcondition_id": "ID-POST-001" ,
+          "prolog": "",
+          "description": ""
+        }
+      ],
+      "system_state":
+       [
+        {
+          "postcondition_id": "ID-POST-002",
+          "prolog": "",
+          "description": ""
+        }
+      ]
+    }
+  ]
+}
